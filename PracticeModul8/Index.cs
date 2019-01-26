e0d8494993735ded543438fc8cf2033c06435226
Nauryzbai <nauzik@mail.ru>
2019-01-26 23:28:02 +0600

    Практика8

---
+++ PracticeModul8/Index.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeModul8
+{
+    public class Index
+    {
+        public int _constantaA;
+        public Index() { }
+        
+        public int [] array;
+        public Index(int constantaA, int constantaB)
+        {
+            SetConstA(constantaA);
+            int constantaC = constantaB - constantaA;
+            array = new int[constantaC];
+        }
+        public void SetConstA(int constantaA) { _constantaA = constantaA; }
+        
+
+        public int this[int index]
+        {
+            get { index = _constantaA; return array[index]; }
+            set { index = _constantaA; array[index] = value; }
+        }
+
+    }
+}
---
+++ PracticeModul8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeModul8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Введите размер массива n X m:");
+            int constantaA = int.Parse(Console.ReadLine());
+            int constantaB = int.Parse(Console.ReadLine());
+            
+            Index ind = new Index(constantaA, constantaB);
+            for (int i = constantaA; i < constantaB; i++)
+            {
+                ind[i] = rand.Next(15, 25);
+                Console.WriteLine(i + " = " + ind[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
